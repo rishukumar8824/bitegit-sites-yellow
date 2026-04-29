ee4bbb96c1170da19d196f25cf0ad23694ac5b19
UrmiIanegi <your-email@gmail.com>
2026-04-29 12:46:41 +0530

    debug: expose actual withdrawal error message in 500 response

--- server.js
+++ server.js
@@ -2432,7 +2432,7 @@ app.post(
       if (error.status) {
         return res.status(error.status).json({ message: error.message });
       }
-      return res.status(500).json({ message: 'Server error while creating withdrawal request.' });
+      return res.status(500).json({ message: 'Server error while creating withdrawal request.', _debug: String(error.message || error.code || error) });
     }
   }
 );
